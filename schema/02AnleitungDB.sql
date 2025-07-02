b3a06e3ed5d9cbfa58ac8a2f8e158a684b848ecd
JMi-B <j.zankl@gmx.de>
2025-07-02 12:58:10 +0200

    01 sql aus 01 mwb. Noch nicht verbessert. einge Tabelen fehlen.02 Tab Anleitung koregiert. Weiter 02 Tab in 01. Fehlende Tabellen. Zusammenhänge

--- schema/02AnleitungDB.sql
+++ schema/02AnleitungDB.sql
@@ -1,11 +1,13 @@
 create database 02AnleitungDB;
 
-create table anleitung (
-idAnleitung int not null auto_increment,
-AnleitungBezeichnung var(255),
-AnleitungNr var(10),
-Seitenzahl int,
-Arbeitsbogen var(10),
-Standort var(45),
-Primary Key (idAnleitung)
-);
+CREATE TABLE anleitung (
+  idAnleitung INT NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+  AnleitungBezeichnung VARCHAR(255) NOT NULL COMMENT 'Bezeichnung der Anleitung',
+  AnleitungNr VARCHAR(10) Comment 'Eindeutige Nummer der Anleitung im Medium',
+  Seitenzahl INT COMMENT 'Seitenzahl im Medium',
+  Arbeitsbogen VARCHAR(10) COMMENT 'Arbeitsbogen-Nummer',
+  Standort VARCHAR(45) COMMENT 'Standort der Anleitung',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (idAnleitung)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
