76a641afb7e5a34ec5454076e8170c15e22c93ee
JMi-B <j.zankl@gmx.de>
2025-07-16 17:38:33 +0200

    Hausaufgabe 16072025

--- schema/testdb.sql
+++ schema/testdb.sql
@@ -3,7 +3,9 @@ use testdb;
 
 /*Kundentabelle anlegen:*/
 
-CREATE or replace TABLE kunden (
+drop table if exists kunden;
+
+CREATE TABLE kunden (
     kundennummer INT AUTO_INCREMENT PRIMARY KEY,
     vorname VARCHAR(100),
     nachname VARCHAR(100),
@@ -11,5 +13,21 @@ CREATE or replace TABLE kunden (
     ort VARCHAR(100)
 );
 
+/* VEraltete Syntax
 GRANT ALL PRIVILEGES ON *.* TO 'pythontest'@'localhost' IDENTIFIED BY 'abcde_12345' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'pythontest'@'%' IDENTIFIED BY 'abcde_12345' WITH GRANT OPTION;
+*/
+
+/*Nutzeranlegen*/
+
+CREATE USER IF NOT EXISTS 'pythontest'@'localhost' IDENTIFIED BY 'abcde_12345';
+CREATE USER IF NOT EXISTS 'pythontest'@'%' IDENTIFIED BY 'abcde_12345';
+
+/*Rechte zuweisen*/
+
+GRANT ALL PRIVILEGES ON *.* TO 'pythontest'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'pythontest'@'%' WITH GRANT OPTION;
+
+/*Rechte neu laden*/
+
+FLUSH PRIVILEGES;
